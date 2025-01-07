340c4380638e07a5dc434c47ff6dd99cb9100383
Daniel Ramírez Morilla <daniel@xarblanca.es>
2025-01-07 19:02:31 +0100

    Move template content

--- Source/HoloBoom/HoloBoomGameMode.cpp
+++ Source/HoloBoom/HoloBoomGameMode.cpp
@@ -8,7 +8,7 @@ AHoloBoomGameMode::AHoloBoomGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/TemplateData/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 }
